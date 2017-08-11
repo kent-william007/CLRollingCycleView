fb2b80d0fbc2f321e4a901b4116d334cd5a6d77d
肖金兴 <xiao-jinxing@sohu.com>
2017-08-11 10:49:08 +0800

    add CLRollingCycleView

---
+++ CLRollingCycleView/class/CLRollingCycleView.h
@@ -0,0 +1,13 @@
+//
+//  CLRollingCycleView.h
+//  CLRollingCycleView
+//
+//  Created by kent on 2017/8/11.
+//  Copyright © 2017年 kent. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CLRollingCycleView : UIView
+
+@end
---
+++ CLRollingCycleView/class/CLRollingCycleView.m
@@ -0,0 +1,21 @@
+//
+//  CLRollingCycleView.m
+//  CLRollingCycleView
+//
+//  Created by kent on 2017/8/11.
+//  Copyright © 2017年 kent. All rights reserved.
+//
+
+#import "CLRollingCycleView.h"
+
+@implementation CLRollingCycleView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
